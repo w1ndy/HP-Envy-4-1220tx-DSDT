d42ed6f16de1834c3a73e23a70733d2ce9ac4f16
w1ndy <mystery.wd@gmail.com>
2015-06-15 14:27:40 +0800

    add clover configuration

--- apply.sh
+++ apply.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 mkdir -p /Volumes/EFI
-sudo mount -t msdos /dev/disk1s1 /Volumes/EFI
+sudo mount -t msdos /dev/disk0s1 /Volumes/EFI
+cp clover/config.plist /Volumes/EFI/EFI/CLOVER/
 iasl dsdt.dsl
 mv dsdt.aml /Volumes/EFI/EFI/CLOVER/acpi/patched/
 sudo diskutil unmount /Volumes/EFI
